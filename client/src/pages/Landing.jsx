985cb81681135259b3d21fb058ab6642b7c5359f
JT Reyes <jesustgreyes@gmail.com>
2023-09-12 12:19:24 -0700

    move signup.jsx and login.jsx

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,13 +2,13 @@
 // When I enter the website for the first time, I want to see a welcome screen
 // On the welcome screen, I see buttons to signup and login
 import {Link} from 'react-router-dom';
-import "client/src/styles/Landing.css";
+import "../styles/Landing.css";
 
 const LandingPage = () => {
     return (
         <div>
             <h1 id="Landing-Header">Welcome to the Landing Page</h1>
-            <Link
+            {/* <Link
             to="/signup"
             >
             <h2>Sign Up</h2>
@@ -18,7 +18,7 @@ const LandingPage = () => {
             to="/login"
             >
             <h2>Log In</h2>
-            </Link>
+            </Link> */}
         </div>
     )
 }
