48def8780a7aba7d95f83c15019d8ca731d0b043
alam2tg <aaronlamfinance@gmail.com>
2023-09-11 18:22:09 -0700

    Finish base styling for blackjack.jsx

--- client/src/pages/Blackjack.jsx
+++ client/src/pages/Blackjack.jsx
@@ -7,54 +7,50 @@ import "../styles/Blackjack.css";
 const Blackjack = () => {
     return (
         <div className="outline blackjack">
-            <h1>Welcome to the Blackjack Page</h1>
+            
+            <h1 className="blackjack-header">Welcome to the Blackjack Page</h1>
             <Link to="/home">
-                <h2>Home</h2>
+                <h2 className="blackjack-nav">Home</h2>
             </Link>
 
-            <section className="game container">
+            <section className="game-container">
 
-                <div className="outline count container">
-                    <h3>Count: </h3>
+                <div className="outline running-count-container small-container">
+                    <h3>Running Count: </h3>
                 </div>
 
-                <div className="score container">
-                    <div className="outline wins">
-                        <h3>Wins:</h3>
-                    </div>
-
-                    <div className="outline losses">
-                        <h3>Losses: </h3>
-                    </div>
-                </div>
-
-                <div className="outline deck">
+                <div className="outline deck-count-container">
                     <h3>Cards in Deck: </h3>
                 </div>
 
-                <div className="outline deal">
-                    <button className="deal-button">
-                        <h3>Deal New Hand</h3>
-                    </button>
+                <div className="score-container ">
+                    <header className="outline wins">
+                        <h3>Wins:</h3>
+                    </header>
+                    <header className="outline losses">
+                        <h3>Losses: </h3>
+                    </header>
                 </div>
 
-                <div className="hit-stand container">
-                    <div className="outline hit">
-                        <button className="hit-button">
-                            <h3>Hit</h3>
-                        </button>
+                <div className="outline deal-container"></div>
+
+                <div className="outline buttons-container">
+                    <div>
+                        <button id="hit-button"><h3>Hit</h3></button>
                     </div>
-                    <div className="outline stand">
-                        <button className="stand-button">
-                            <h3>Stand</h3>
-                        </button>
+                    <div>
+                        <button id="stand-button"><h3>Stand</h3></button>
+                    </div>
+                    <div>
+                        <button id="deal-button"><h3>Deal New Hand</h3></button>
                     </div>
                 </div>
 
-                <div className="outline dealer-cards container">
+
+                <div className="outline dealer-cards-container">
                     <h3>Dealer Cards Container</h3>
                 </div>
-                <div className="outline player-cards">
+                <div className="outline player-cards-container">
                     <h3>Player Cards Container</h3>
                 </div>
             </section>
