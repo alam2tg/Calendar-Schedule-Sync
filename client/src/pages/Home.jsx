2379b09ca695248807cc5a0f191a8a0cf4bb6625
JT Reyes <jesustgreyes@gmail.com>
2023-09-12 21:37:27 -0700

    add content to home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,23 +3,25 @@
 // when I click the tutorial button, then the intro.js tutorial is rendered
 // As a user, when I click the start button the blackjack page is rendered
 
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 import '../styles/Home.css'
 
 const HomePage = () => {
     return (
         <div>
             <h1>Welcome to the Home Page</h1>
-            <Link
-            to="/blackjack"
-            >
-            <h2>Start</h2>
-            </Link>
-            <Link
-            to="/tutorial"
-            >
-            <h2>Tutorial</h2>
-            </Link>
+            <h2>New to Blackjack?</h2>
+            <h3>Blackjack, also known as 21, is a popular card game played in casinos and homes worldwide. 
+                The objective is simple: players aim to beat the dealer by getting a hand value as close to 
+                21 as possible without exceeding it. Each card has a point value; numbered cards are worth 
+                their face value, face cards (kings, queens, and jacks) are worth 10 points, and aces can be 
+                worth either 1 or 11 points, depending on what benefits the player&apos;s hand. Players are dealt 
+                two cards initially and can choose to &quot;hit&quot; (take another card) to improve their hand or 
+                &quot;stand&quot; (keep their current hand). The dealer also receives two cards but only one is face-up. 
+                Players can win by having a hand totaling 21, getting a higher hand value than the dealer without 
+                going over 21, or having the dealer bust (exceed 21). However, if a player&apos;s hand exceeds 21, 
+                they lose, regardless of the dealer&apos;s hand. Blackjack combines strategy and luck, making it an 
+                exciting and challenging game for players of all skill levels.</h3>
         </div>
     )
 }
