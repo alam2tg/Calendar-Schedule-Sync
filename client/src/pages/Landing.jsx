3d1ea73a2dd776a161f9dcf8f90935ae8f8059f4
frausto98 <frausto9298@yahoo.com>
2023-09-13 14:53:34 -0700

    added landing page login functionality

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -6,18 +6,27 @@
 // import React from 'react';
 import "../styles/Landing.css";
 
+import Auth from  "../utils/auth"
+
 const LandingPage = () => {
     return (
       <div className="landing-page">
         <nav className="landing-navbar">
+            { Auth.loggedIn() ? (
           <ul className="landing-nav-list">
             <li className="landing-nav-item"><a href="/Home">Home</a></li>
             <li className="landing-nav-item"><a href="/Blackjack">Play Blackjack</a></li>
             <li className="landing-nav-item"><a href="/Tutorial">Tutorial</a></li>
           </ul>
+
+            ):(
+            <>
+            <h2> Login! </h2>
+            </>
+            )}
         </nav>
         <div className="landing-content">
-          <h1 className="landing-title">Welcome to the Landing Page</h1>
+          <h1 className="landing-title">Welcome to</h1>
           <img src="src/assets/images/black-jack-logo.png" alt="Blackjack Logo" className="logo" />
         </div>
       </div>
