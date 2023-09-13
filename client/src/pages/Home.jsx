2a52568f03c47b6992be861c0601ea8bfd5fffdb
JT Reyes <jesustgreyes@gmail.com>
2023-09-12 22:01:55 -0700

    escape quotes and parentheses on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,14 @@ const HomePage = () => {
         </div>
         <div className="sub-content">
             <p className="subtitle">New to Blackjack?</p>
-            <p className="description">Blackjack, also known as 21, is a popular card game played in casinos and homes worldwide. The objective is simple: players aim to beat the dealer by getting a hand value as close to 21 as possible without exceeding it. Each card has a point value; numbered cards are worth their face value, face cards (kings, queens, and jacks) are worth 10 points, and aces can be worth either 1 or 11 points, depending on what benefits the player's hand. Players are dealt two cards initially and can choose to "hit" (take another card) to improve their hand or "stand" (keep their current hand). The dealer also receives two cards but only one is face-up. Players can win by having a hand totaling 21, getting a higher hand value than the dealer without going over 21, or having the dealer bust (exceed 21). However, if a player's hand exceeds 21, they lose, regardless of the dealer's hand. Blackjack combines strategy and luck, making it an exciting and challenging game for players of all skill levels.</p>
+            <p className="description">Blackjack, also known as 21, is a popular card game played in casinos and homes worldwide. 
+            The objective is simple: players aim to beat the dealer by getting a hand value as close to 21 as possible without exceeding it. 
+            Each card has a point value; numbered cards are worth their face value, face cards (kings, queens, and jacks) are worth 10 points, 
+            and aces can be worth either 1 or 11 points, depending on what benefits the player&apos;s hand. Players are dealt two cards initially and 
+            can choose to &quot;hit&quot; (take another card) to improve their hand or &quot;stand&quot; (keep their current hand). The dealer also receives two 
+            cards but only one is face-up. Players can win by having a hand totaling 21, getting a higher hand value than the dealer without going 
+            over 21, or having the dealer bust (exceed 21). However, if a player&apos;s hand exceeds 21, they lose, regardless of the dealer&apos;s hand. 
+            Blackjack combines strategy and luck, making it an exciting and challenging game for players of all skill levels.</p>
         </div>
       </div>
     );
